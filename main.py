000feb2783f45e952492c516e352f2dab618d5b1
Ihr Name auf github <hl5162@mci4me.at>
2024-06-03 10:04:33 +0200

    bild, vor-nachname, alter und maxhr anzeigen

--- main.py
+++ main.py
@@ -10,24 +10,34 @@ if 'current_user' not in st.session_state:
     st.session_state.current_user = 'None'
 
 st.write("# EKG APP") # Eine Überschrift der ersten Ebene
-st.write("## Versuchsperson auswählen") # Eine Überschrift der zweiten Ebene
+st.write("### Versuchsperson auswählen") # Eine Überschrift der zweiten Ebene
 
-# Dieses Mal speichern wir die Auswahl als Session State
 st.session_state.current_user = st.selectbox(
     'Versuchsperson',
     options = person_names_list, key="sbVersuchsperson")
 
 # Finden der Person - den String haben wir im Session state
 current_person = Person.find_person_data_by_name(st.session_state.current_user)
+current_person_obj = Person(current_person) # Erstellen eines Person-Objekts aus dem Dictionary
 
-"""picture_path ist ein Key im Dictionary, der den Pfad zum Bild enthält"""
-image = Image.open(current_person["picture_path"]) # Bild laden und # Auslesen des Pfades aus dem zurückgegebenen Dictionary
+image = Image.open(current_person_obj.picture_path) # Bild laden und Auslesen des Pfades aus dem zurückgegebenen Dictionary
 
-st.image(image, caption = st.session_state.current_user) # Bild anzeigen lassen
+# Bild und Informationen nebeneinander anzeigen
+col1, mid, col2 = st.columns([1,10,20])
+with col1:
+    st.image(image, width=250)
+with col2:
+    st.write('**Vorname:**', current_person_obj.firstname)
+    st.write('**Nachname:**', current_person_obj.lastname)
+    st.write('**Alter:**', current_person_obj.age)
+    st.write('**Max HR:**', current_person_obj.max_hr_bpm)
 
-st.write("Name der ausgewählten Versuchsperson: ", st.session_state.current_user) 
+
+#st.image(image, caption = st.session_state.current_user) # Bild anzeigen lassen --> wurde ersetzt
+#st.write("Name der ausgewählten Versuchsperson: ", st.session_state.current_user) --> wurde erstezt 
 
 
 '''Drop down mit ID / Datum um EKG Daten zu laden und anzuzeigen
    wieder eine Liste erstellen der EKGs wie bei den Personen mit Vorname und Nachname
-   '''
+'''
+
