66712e3cf15820b46663b58c5677784910070207
kf7091 <kf7091@mci4me.at>
2024-06-03 10:11:38 +0200

    add functional find_peaks

--- ekgdata.py
+++ ekgdata.py
@@ -1,5 +1,6 @@
 import json
 import pandas as pd
+import plotly.graph_objects as go
 
 # Klasse EKG-Data für Peakfinder, die uns ermöglicht peaks zu finden
 
@@ -16,27 +17,24 @@ class EKGdata:
     def load_by_id(self, id):
         return {'id': id, 'date': self.date, 'result_link': self.data}
 
-    def find_peaks(series, threshold, respacing_factor=5):
+    def find_peaks(self, threshold:float, respacing_factor:int=5):
         """
         A function to find the peaks in a series
         Args:
-            - series (pd.Series): The series to find the peaks in
             - threshold (float): The threshold for the peaks
             - respacing_factor (int): The factor to respace the series
         Returns:
             - peaks (list): A list of the indices of the peaks
         """
-        ### how does this work?
-
         
         # Respace the series
-        series = series.iloc[::respacing_factor]
+        series = self.df["EKG in mV"].iloc[::respacing_factor]
     
         # Filter the series
         series = series[series>threshold]
 
 
-        peaks = []
+        self.peaks = []
         last = 0
         current = 0
         next = 0
@@ -47,23 +45,51 @@ class EKGdata:
             next = row
 
             if last < current and current > next and current > threshold:
-                peaks.append(index-respacing_factor)
+                self.peaks.append(index-respacing_factor)
 
-        return peaks
+        return self.peaks
     
     def estimate_hr(self):
-        pass
+        ''' Estimate the heart rate from the peaks found in the EKG data'''
+        if not hasattr(self, 'peaks'):
+            raise ValueError("No peaks found - please run find_peaks() first")
+        else:
+            print(self.peaks[:1])
+            hr = pd.Series("HR", name="HR", index=self.peaks)
+            for i in range(1, len(self.peaks)):
+                hr.at[i, 'HR'] = 1/((self.peaks[i] - self.peaks[i-1])/60000)
+        return hr
+
+        
     def plot_time_series():
         pass
 
 
 if __name__ == "__main__":
     print("This is a module with some functions to read the EKG data")
+
+    print('Loading Data')
     file = open("data/person_db.json")
     person_data = json.load(file)
+
+    print('convert to dict')
     ekg_dict = person_data[0]["ekg_tests"][0]
     print(ekg_dict)
+
+    print('create EKGdata object')
     ekg = EKGdata(ekg_dict)
     print(ekg.df.head())
+    print(type(ekg))
+    print(type(ekg_dict))
 
-    peaks = ekg.find_peaks(ekg.df['EKG in mV'], 250)
+    print('find peaks')
+    ekg.find_peaks(250)
+    print(ekg.peaks[:10])
+
+    print('estimate hr')
+    print(ekg.estimate_hr()[:10])
+
+    fig = go.Figure(data=go.Scatter(x=ekg.df["Time in ms"], y=ekg.df["EKG in mV"]))
+    #add_trace = go.Scatter(x=ekg.df["Time in ms"].iloc[ekg.peaks], y=ekg.df["EKG in mV"].iloc[ekg.peaks], mode='markers', marker=dict(color='red', size=8))
+    #fig.add_trace(add_trace)
+    fig.show()
