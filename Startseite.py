035ed5f502091d7f48e9e9adada24320feeb32ae
LukiHopf <hl5162@mci4me.at>
2024-07-09 21:06:44 +0200

    pages hinzugefügt

---
+++ Startseite.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+# st.sidebar.header("Startseite und Login")
+
+st.write("# Hallo zu unserer App! Bitte loggen Sie sich ein.")
+
