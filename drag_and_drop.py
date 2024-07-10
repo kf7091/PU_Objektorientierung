197a997189b57dc54ae0708126def389413c447a
LukiHopf <hl5162@mci4me.at>
2024-07-10 22:28:17 +0200

    Daten löschen fertig

--- drag_and_drop.py
+++
@@ -1,1 +0,0 @@
-uploaded_picture = st.file_uploader("Bild hochladen", type="jpg", accept_multiple_files=False, key="uploaded_picture")
--- pages/5_Daten_löschen.py
+++ pages/5_Daten_löschen.py
@@ -7,51 +7,76 @@ if 'logged_in' not in st.session_state or not st.session_state.logged_in:
     st.warning("Bitte loggen Sie sich zuerst ein!")
     st.stop()
 
+if 'clicked' not in st.session_state:
+    st.session_state.clicked = False
+
+def click_button():
+    st.session_state.clicked = True
+
 # Laden der Personendaten
 person_data = Person.load_person_data()
 person_names_list = Person.get_persons_list(person_data)
 
-# Auswahl der zu löschenden Person
-st.title("Person und zugehörige EKG-Daten löschen")
-selected_person = st.selectbox(
-    "Wählen Sie eine Person zum Löschen aus",
-    options=person_names_list,
-    key="delete_person"
-)
+tab1, tab2 = st.tabs(["Person löschen", "EKG-Test löschen"])
 
-if selected_person:
+with tab1:
+    # Auswahl der zu löschenden Person
+    st.title("Person und zugehörige EKG-Daten löschen")
+    selected_person = st.selectbox(
+        "Wählen Sie eine Person zum Löschen aus",
+        options=person_names_list,
+        key="delete_person"
+    )
     person_data = Person.find_person_data_by_name(selected_person)
     person_id = int(Person.find_person_id_by_name(selected_person))
+    st.button("Person und zugehörie EKG-Tests löschen!", on_click=click_button)    
 
-    if st.button("Person und zugehörige EKG-Tests löschen"):
-        # Löschen der Person und der zugehörigen EKG-Tests
-        Person.delete_person(person_id)
-        st.success(f"Person {selected_person} und alle zugehörigen EKG-Tests wurden gelöscht.")
+    # Löschen der Person und der zugehörigen EKG-Tests
+    if st.session_state.clicked:
+        if st.button("Ja, Person löschen"):
+            Person.delete_person(person_id)
+            st.success(f"Person {selected_person} und alle zugehörigen EKG-Tests wurden gelöscht.")
+            st.session_state.clicked = False
+            st.stop()
+        if st.button("Nein, Person löschen abbrechen"):    
+            st.info("Löschen abgebrochen.")
+            st.session_state.clicked = False
+            st.stop()
 
-# Auswahl der zu löschenden EKG-Tests
-st.title("EKG-Daten löschen")
-st.session_state.selected_person = st.selectbox(
-    "Wählen Sie eine Person aus",
-    options=person_names_list,
-    key="person_of_delete_ekg"
-)
+with tab2:
+    # Auswahl der zu löschenden EKG-Tests
+    st.title("EKG-Daten löschen")
+    st.session_state.selected_person = st.selectbox(
+        "Wählen Sie eine Person aus",
+        options=person_names_list,
+        key="person_of_delete_ekg"
+    )
 
-# Finden der Person - den String haben wir im Session state
-current_person = Person.find_person_id_by_name(st.session_state.selected_person)
-current_person_obj = Person(current_person) # Erstellen eines Person-Objekts aus dem Dictionary
-person_ekg_list = EKGdata.get_ekgids_by_personid(current_person_obj.id) # Erstellen einer Liste von EKGs der gewählten Person
+    # Finden der Person - den String haben wir im Session state
+    current_person = Person.find_person_id_by_name(st.session_state.selected_person)
+    current_person_obj = Person(current_person) # Erstellen eines Person-Objekts aus dem Dictionary
+    person_ekg_list = EKGdata.get_ekgids_by_personid(current_person_obj.id) # Erstellen einer Liste von EKGs der gewählten Person
 
-selected_ekg = st.selectbox(
-    "Wählen Sie einen EKG-Test zum Löschen aus",
-    options=person_ekg_list,
-    key="delete_ekg"
-)
+    selected_ekg = st.selectbox(
+        "Wählen Sie einen EKG-Test zum Löschen aus",
+        options=person_ekg_list,
+        key="delete_ekg"
+    )
 
-#if selected_ekg:
- #   ekg_data = EKGdata.find_person_data_by_name(selected_person)
-  #  ekg_id = int(EKGdata.find_person_id_by_name(selected_person))
+    #if selected_ekg:
+    #   ekg_data = EKGdata.find_person_data_by_name(selected_person)
+    #  ekg_id = int(EKGdata.find_person_id_by_name(selected_person))
 
-if st.button("EKG-Test löschen"):
-# Löschen der Person und der zugehörigen EKG-Tests
-    EKGdata.delete_ekg_file(person_id)
-    st.success(f"EKG-Test {selected_ekg} wurde gelöscht.")
+    st.button("EKG-Test löschen", on_click=click_button)
+        # Löschen des EKG-Tests
+    if st.session_state.clicked:
+        if st.button("Ja, EKG löschen"):
+            EKGdata.delete_ekg_file(person_id)
+            st.success(f"EKG-Test {selected_ekg} wurde gelöscht.")
+            st.session_state.clicked = False
+            st.stop()
+        if st.button("Nein, EKG löschen abbrechen"):
+            st.info("Löschen abgebrochen.")
+            st.session_state.clicked = False
+            st.stop()
+
