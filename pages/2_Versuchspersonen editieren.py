0c4717e130a31a2d2ebd0b2463256e206fed3f22
kf7091 <kf7091@mci4me.at>
2024-07-10 22:21:54 +0200

    put user edits together

--- pages/2_Versuchspersonen editieren.py
+++ pages/2_Versuchspersonen editieren.py
@@ -10,7 +10,9 @@ if 'logged_in' not in st.session_state or not st.session_state.logged_in:
     st.warning("Bitte loggen Sie sich zuerst auf der Startseite ein oder erstellen Sie einen neuen Benutzer!")
     st.stop()
 
-tab1, tab2 = st.tabs(["Personen hinzufügen", "Bild hochladen"])
+person_names_list = Person.get_persons_list(TinyDB(os.path.join("data", "person_db.json")).table("persons"))
+
+tab1, tab2, tab3 = st.tabs(["Personen hinzufügen", "Person bearbeiten", "Bild hochladen"])
 with tab1:
     st.title("Personen hinzufügen")
 
@@ -23,14 +25,35 @@ with tab1:
     # Button zum Hinzufügen der Person
     if st.button("Person hinzufügen"):
         if firstname and lastname and year_of_birth:
-            picture_path = "data/pictures/empty.png"
-            Person.add_person(firstname, lastname, year_of_birth, picture_path)
+            Person.add_person(firstname, lastname, year_of_birth)
             st.success(f"Person {firstname} {lastname} hinzugefügt")
         else:
             st.error("Bitte geben Sie einen Vornamen, Nachnamen und das Geburtsjahr ein!")
 with tab2:
+    st.title("Personendaten bearbeiten")
 
-    person_names_list = Person.get_persons_list(TinyDB("data/person_db.json").table("persons"))
+    selected_person = st.selectbox(
+        "Wählen Sie eine Person zum Bearbeiten aus",
+        options=person_names_list,
+        key="edit_person"
+    )
+
+    if selected_person:
+        person_data = Person.find_person_data_by_name(selected_person)
+        person_id = int(Person.find_person_id_by_name(selected_person))
+        person_info = person_data
+
+        # Eingabefelder für die Personendaten
+        firstname = st.text_input("Vorname", value=person_info['firstname'])
+        lastname = st.text_input("Nachname", value=person_info['lastname'])
+        year_of_birth = st.number_input("Geburtsjahr", value=person_info['year_of_birth'], format="%d")
+
+        if st.button("Personendaten aktualisieren"):
+            # Aktualisieren der Personendaten
+            Person.update_person(person_id, firstname, lastname, year_of_birth, picture_path)
+            st.success(f"Personendaten für {firstname} {lastname} wurden aktualisiert.")
+with tab3:
+    st.title("Bild hochladen")
 
     if 'current_user' not in st.session_state:
         st.session_state.selected_person = 'None'
@@ -40,7 +63,7 @@ with tab2:
         options = person_names_list, key="sbVersuchsperson")
 
     person_id = Person.find_person_id_by_name(st.session_state.selected_person)
-    path = "data" + os.sep + "person_pictures" + os.sep + '{}.jpg'.format(person_id)
+    path = os.path.join("data", "person_pictures", '{}.jpg'.format(person_id))
     
     #uploaded_picture = st.file_uploader("Bild hochladen", type=["png", "jpg", "jpeg"], accept_multiple_files=False, key="uploaded_picture")
     uploaded_picture = st.file_uploader("Bild hochladen", type="jpg", accept_multiple_files=False, key="uploaded_picture")
